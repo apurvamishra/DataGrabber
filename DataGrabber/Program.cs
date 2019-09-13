570bd38c74fc934ca4bdf883b2afc17c45a244d4
apurvamishra <apurva.mishra@outlook.com>
2019-09-13 13:17:22 +1000

    A lot

--- DataGrabber/Program.cs
+++ DataGrabber/Program.cs
@@ -9,6 +9,8 @@ using System.Reactive.Linq;
 using System.IO;
 using System.Xml.Linq;
 using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Globalization;
 
 namespace DataGrabber
 {
@@ -55,8 +57,10 @@ namespace DataGrabber
                 var result = beamDBscada.ReadFromDB(dbNumber);
                 if (result != 0)
                 {
-                    Console.WriteLine("\n" + "...cannot read from PLC database, press anykey to exit and then restart application.");
-                    Console.ReadKey();
+                    Console.WriteLine("\n" + "...cannot read from PLC database, trying to reconnect.");
+                    JPLCConnection.Instance.Connect();
+                    Console.WriteLine($"PLC Connected = {JPLCConnection.Instance.Connected}");
+                    //Console.ReadKey();
                     return beamDBscada;
                 }
                 else
@@ -70,7 +74,7 @@ namespace DataGrabber
                 //===============================================================================
                 // THIS SECTION OF CODE WILL REPEAT
                 Console.WriteLine("\n" + $"Reading from PLC DataBase on {DateTime.Now}");
-                Console.WriteLine("Writing to location: " + AppDomain.CurrentDomain.BaseDirectory + "OPF.csv");
+                
                 //Current data stream from PLC
                 Console.WriteLine("   Beam Parameters: " + "\n" +
                                   "     BeamID - " + $"{yourdb.Beam.Value.Beam_Parameters.Value.ID.Value}" + "\n" +
@@ -87,19 +91,6 @@ namespace DataGrabber
                                   "     Beam rejected ? " + $"{yourdb.Beam.Value.Beam_Reject.Value}" + "\n" +
                                   "     Reject reason - " + $"{yourdb.Beam.Value.Beam_Reject_Reason.Value}" + "\n" +
                                   "     Beam Processed Timestamp - " + $"{yourdb.Beam.Value.Beam_Processing_Complete_Timestamp.Value}" + "\n" +
-                                  "   Beam's different Stages Data: " + "\n" +
-                                  "     For welding RH Lock: beam on jig ? " + $"{yourdb.Beam.Value.Beam_Process_Stage.Value.weld_1_beam_on_jig.Value}" + "\n" +
-                                  "     For welding RH Lock: beam on jig number " + $"{yourdb.Beam.Value.Beam_Process_Stage.Value.weld_1_beam_on_jig_number.Value}" + "\n" +
-                                  "     Welding RH Lock: in progress ? " + $"{yourdb.Beam.Value.Beam_Process_Stage.Value.weld_1_in_progress.Value}" + "\n" +
-                                  "     Welding RH Lock: completed ? " + $"{yourdb.Beam.Value.Beam_Process_Stage.Value.weld_1_complete.Value}" + "\n" +
-                                  "     Progressing to weld 2 ? " + $"{yourdb.Beam.Value.Beam_Process_Stage.Value.moving_to_weld_2.Value}" + "\n" +
-                                  "     For welding LH Lock: beam on jig ? " + $"{yourdb.Beam.Value.Beam_Process_Stage.Value.weld_2_beam_on_jig.Value}" + "\n" +
-                                  "     For welding LH Lock: beam on jig number " + $"{yourdb.Beam.Value.Beam_Process_Stage.Value.weld_2_beam_on_jig_number.Value}" + "\n" +
-                                  "     Welding LH Lock: in progress ? " + $"{yourdb.Beam.Value.Beam_Process_Stage.Value.weld_2_in_progress.Value}" + "\n" +
-                                  "     Welding LH Lock: completed ? " + $"{yourdb.Beam.Value.Beam_Process_Stage.Value.weld_2_complete.Value}" + "\n" + 
-                                  "     Beam moving towards stillage ? " + $"{yourdb.Beam.Value.Beam_Process_Stage.Value.moving_to_stillage.Value}" + "\n" +
-                                  "     Beam moving towards paintline ? " + $"{yourdb.Beam.Value.Beam_Process_Stage.Value.moving_to_paintline.Value}" + "\n" +
-                                  "     Beam moving towards reject bin ? " + $"{yourdb.Beam.Value.Beam_Process_Stage.Value.moving_to_reject.Value}" + "\n" +
                                   "     Beam at stillage ? " + $"{yourdb.Beam.Value.Beam_Process_Stage.Value.beam_at_stillage.Value}" + "\n" +
                                   "     Beam at paintline ? " + $"{yourdb.Beam.Value.Beam_Process_Stage.Value.beam_on_paintline.Value}" + "\n" +
                                   "     Beam at reject bin ? " + $"{yourdb.Beam.Value.Beam_Process_Stage.Value.beam_at_reject.Value}" + "\n"
@@ -109,7 +100,7 @@ namespace DataGrabber
                 StringBuilder header = new StringBuilder();
                 StringBuilder entries = new StringBuilder();
                 //CSV Path and filename
-                string fileNamePath = AppDomain.CurrentDomain.BaseDirectory + "\\OPF.csv";
+                string fileNamePath = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\OPF.csv";
 
                 //If file doesn't exit create one and add header
                 if (!File.Exists(fileNamePath))
@@ -135,12 +126,47 @@ namespace DataGrabber
                 string currentBeamID = System.Convert.ToString(yourdb.Beam.Value.Beam_Parameters.Value.ID.Value);
                 string lastCSVEntry = File.ReadLines(fileNamePath).Last();
                 string[] lastCSVBeamID = lastCSVEntry.Split(',');
-                Console.WriteLine("Checking for new Beam ID....");
-                Console.WriteLine((lastCSVBeamID[0] == "id" ? "Old ID = None " : "Old ID = " + lastCSVBeamID[0]));
-                Console.WriteLine("Current ID = " + currentBeamID);
+                Console.WriteLine("Checking for new Beam ID in CSV....");
+                Console.WriteLine((lastCSVBeamID[0] == "id" ? "Old Beam ID in CSV file = None " : "Old Beam ID in CSV file = " + lastCSVBeamID[0]));
+                Console.WriteLine("Current Beam ID = " + currentBeamID);
                 //Check for new beam ID generated 
                 bool newEntryResult = currentBeamID.Equals(lastCSVBeamID[0]);
-                Console.WriteLine($" ....new ID {(newEntryResult ? "Not Detected!" : "Detected. Storing in database .... " + AddBeamDatatoDatabase())}");
+                Console.WriteLine($" ....new ID {(newEntryResult ? "Not Detected!" : "Detected. Storing in CSV .... " + AddBeamDatatoCSV())}");
+
+                //Check if the Beam ID from current data stream is different to the last entry in SQL Database
+                //Establish connection to database
+                string connetionString;
+                SqlConnection cnn;
+                connetionString = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=BeamDatabase;Integrated Security=True";
+                cnn = new SqlConnection(connetionString);
+                cnn.Open();
+                Console.WriteLine("\n" + "Connection to Database ....");
+                
+                //Read last entry from Database 
+                SqlCommand command;
+                //DataReader object is used to get all the data specified by the SQL query. We can then read all the table rows one by one using the data reader.
+                SqlDataReader dataReader;
+                string lastDBEntryResult = "";
+                string lastDBEntry = "SELECT BeamID FROM Beams WHERE BeamID not in (SELECT TOP (SELECT COUNT(1)-1 FROM Beams) BeamID FROM Beams)";
+                command = new SqlCommand(lastDBEntry, cnn);
+                dataReader = command.ExecuteReader();
+                //reading data row by row if needed  change GetValue to (0)(1)(2)...
+                while (dataReader.Read())
+                {
+                    lastDBEntryResult = Convert.ToString(dataReader.GetValue(0));
+                };
+                dataReader.Close();
+                command.Dispose();
+
+                //Check if the Beam ID from current data stream is different to the last entry in Database
+                Console.WriteLine("Checking for new Beam ID in Database ....");
+                Console.WriteLine((lastDBEntryResult == null ? "Old Beam ID in Database = None " : "Old Beam ID in Database = " + lastDBEntryResult));
+                Console.WriteLine("Current Beam ID = " + currentBeamID); 
+                newEntryResult = currentBeamID.Equals(lastDBEntryResult);
+                Console.WriteLine($" ....new ID {(newEntryResult ? "Not Detected!" : "Detected. Storing in Database .... " + AddBeamDatatoDatabase())}");
+                //Close DB connection and dispose of all temp objects created
+                command.Dispose();
+                cnn.Close();
 
                 //sub-programs
                 void CreateFileWithHeaderOrAddHeader()
@@ -177,7 +203,7 @@ namespace DataGrabber
                                           );
                     File.AppendAllText(fileNamePath, header.ToString());
                 }
-                string AddBeamDatatoDatabase()
+                string AddBeamDatatoCSV()
                 {
                     entries.AppendLine(
                                                yourdb.Beam.Value.Beam_Parameters.Value.ID.Value + "," +
@@ -210,18 +236,13 @@ namespace DataGrabber
                                                yourdb.Beam.Value.Beam_Process_Stage.Value.beam_at_reject.Value
                                               );
                     File.AppendAllText(fileNamePath, entries.ToString());
-                    //Establish connection to database
-                    string connetionString;
-                    SqlConnection cnn;
-                    connetionString = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=BeamDatabase;Integrated Security=True";
-                    cnn = new SqlConnection(connetionString);
-                    cnn.Open();
-                    Console.WriteLine("Connection to Database open....");
-                    
+                    Console.WriteLine("Writing to location: " + System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\OPF.csv");
 
+                    return "Done!";
+                }
+                string AddBeamDatatoDatabase()
+                {
                     //Write to Database
-                    SqlCommand command;
-                    SqlDataAdapter adapter = new SqlDataAdapter();
                     string sql = "";
                     sql = ("INSERT INTO Beams (" +
                                           "BeamID" + "," +
@@ -266,7 +287,9 @@ namespace DataGrabber
                                             yourdb.Beam.Value.Weld2_Score.Value + "," +
                                             "\'" + yourdb.Beam.Value.Beam_Reject.Value + "\'" + "," +
                                             "\'" + yourdb.Beam.Value.Beam_Reject_Reason.Value + "\'" + "," +
-                                            "\'" + yourdb.Beam.Value.Beam_Processing_Complete_Timestamp.Value + "\'" + "," +
+                                            // date format in SQL DB 09/13/2019 9:17:56 AM - US
+                                            // date format from PLC 13/09/2019 9:17:56 AM - British
+                                            "\'" + yourdb.Beam.Value.Beam_Processing_Complete_Timestamp.Value.ToString("G", new CultureInfo("en-US")) + "\'" + "," +
                                             "\'" + yourdb.Beam.Value.Beam_Process_Stage.Value.weld_1_beam_on_jig.Value + "\'" + "," +
                                             yourdb.Beam.Value.Beam_Process_Stage.Value.weld_1_beam_on_jig_number.Value + "," +
                                             "\'" + yourdb.Beam.Value.Beam_Process_Stage.Value.weld_1_in_progress.Value + "\'" + "," +
@@ -284,15 +307,16 @@ namespace DataGrabber
                                             "\'" + yourdb.Beam.Value.Beam_Process_Stage.Value.beam_at_reject.Value + "\'" +
                                             ")");
                     Console.WriteLine(sql);
+
+                    //DataAdapter object is used to perform specific SQL operations such as insert, delete and update commands
+                    SqlDataAdapter adapter = new SqlDataAdapter();
                     command = new SqlCommand(sql, cnn);
+                    //we now associate the insert SQL command to our adapter
                     adapter.InsertCommand = new SqlCommand(sql, cnn);
+                    //then issue the ExecuteNonQuery method which is used to execute the Insert statement against our database
                     adapter.InsertCommand.ExecuteNonQuery();
-
-                    command.Dispose();
-                    cnn.Close();
                     return "Done!";
-                }
-
+                };
             });
 
 
